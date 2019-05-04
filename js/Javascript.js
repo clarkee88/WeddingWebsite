897400dd40b8f78fc8ff3c37d3367871907c903e
Elliott Clarke <Ell@Elliotts-iMac.local>
2019-05-04 19:54:40 +0100

    Changed Date/time box and added slideshow for venue section

---
+++ js/Javascript.js
@@ -0,0 +1,19 @@
+var images = [
+    "Img/shustoke.jpg",
+    "Img/shustoke2.jpg",
+    "Img/shustoke3.jpg",
+    "Img/shustoke4.jpg",
+    "Img/shustoke5.jpg",
+    "Img/shustoke6.jpg",
+  ]
+
+var imageHead = document.getElementById("venue-section");
+
+var i = 0;
+setInterval(function() {
+      imageHead.style.backgroundImage = "url(" + images[i] + ")";
+      i = i + 1;
+      if (i == images.length) {
+        i =  0;
+      }
+}, 5000);
